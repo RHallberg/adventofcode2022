d8e67999fc24a454162f690b1db2153358aff71d
RHallberg <richal@bredband2.se>
2022-12-15 11:09:47 +0100

    Golfed day 6 solution

--- solution_6.rb
+++ solution_6.rb
@@ -1,25 +1,13 @@
 
 
 def solve(f)
-
-  input = File.read(f).split("")
-
-  counter = 0
-  input.each_with_index do |_, index|
-    puts input[index..index+3].join
+  File.read(f).split("").each_with_index do |_, index|
     return index+4 if input[index..index+3].uniq.count == 4
   end
-
 end
 
 def solve_pt2(f)
-
-  input = File.read(f).split("")
-
-  counter = 0
-  input.each_with_index do |_, index|
-    puts input[index..index+13].join
+  File.read(f).split("").each_with_index do |_, index|
     return index+14 if input[index..index+13].uniq.count == 14
   end
-
 end
